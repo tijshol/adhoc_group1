7f2bbae464cf9af87e7b1474e2a442d19b4b7b8e
tijshol <tijs.hol@gmail.com>
2016-11-17 17:39:49 +0100

    Fix Highlights
    Weight was not displaying

--- highlight.m
+++ highlight.m
@@ -8,7 +8,7 @@ function highlight( route )
     for i = 1:length(route)-1
         plot([node(route(i)).pos(1) node(route(i+1)).pos(1)], ...
              [node(route(i)).pos(2) node(route(i+1)).pos(2)], ...
-             'Color',col,'LineWidth',4-3*(link(i,i+1).weight/max([link.weight])));
+             'Color',col,'LineWidth',4-3*(link(route(i),route(i+1)).weight/max([link.weight])));
     end
     for i = 1:length(route)
         plot(node(route(i)).pos(1),node(route(i)).pos(2),'.','Color',col,'MarkerSize',40);
